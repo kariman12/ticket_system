6345b028d991559412a78a5d4cffd00146e27dd1
kariman12 <marika.nakano.1212@gmail.com>
2020-09-21 00:01:05 +0900

    add percent display

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,10 @@ class HomeController < ApplicationController
   before_action :forbid_login_user, {only: [:about]}
 
   def top
+    @all_tickets = Ticket.all.count
+    @request_tickets = Ticket.where(status: "配布待ち").count
+    @get_tickets = Ticket.where(status: "配布済").count
+    @sold_tickets = Ticket.where(status: "販売完了").count
   end
 
   def about
