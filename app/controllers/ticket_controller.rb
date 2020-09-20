f71bd6d7bfbfba69129a56d46d28fdcdd404f995
kariman12 <marika.nakano.1212@gmail.com>
2020-09-20 15:39:22 +0900

    enable to add today

--- app/controllers/ticket_controller.rb
+++ app/controllers/ticket_controller.rb
@@ -6,7 +6,7 @@ class TicketController < ApplicationController
   def create
     ticket_num = params[:ticketNum].to_i
     for i in 1..ticket_num
-      @ticket = Ticket.new(status:"配布待ち", request_dates:"2020/09/01", get_dates:"", sold_dates:"")
+      @ticket = Ticket.new(status:"配布待ち", request_dates:Date.today, get_dates:"", sold_dates:"")
       @ticket.save
     end
     redirect_to("/mypage")
