aa3091cdcd3d714e7b71be1cfbf804afbea97c52
kariman12 <marika.nakano.1212@gmail.com>
2020-09-20 15:31:18 +0900

    change ticket request post

--- app/controllers/ticket_controller.rb
+++ app/controllers/ticket_controller.rb
@@ -4,7 +4,12 @@ class TicketController < ApplicationController
   end
 
   def create
-    
+    ticket_num = params[:ticketNum].to_i
+    for i in 1..ticket_num
+      @ticket = Ticket.new(status:"配布待ち", request_dates:"2020/09/01", get_dates:"", sold_dates:"")
+      @ticket.save
+    end
+    redirect_to("/mypage")
   end
 
   def mypage
