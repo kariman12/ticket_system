753a239accec353cab87974a87b8cd78e3f77e66
kariman12 <marika.nakano.1212@gmail.com>
2020-09-20 21:39:58 +0900

    add bcrypt gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
+    has_secure_password
     validates :name, {presence: true, uniqueness: true}
     validates :part, {presence: true}
     validates :generation, {presence: true}
-    validates :password, {presence: true}
 end
