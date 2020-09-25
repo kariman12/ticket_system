386364c3c9d42463ad6aba78cfc1ed2b72918cd1
kariman12 <marika.nakano.1212@gmail.com>
2020-09-26 02:05:02 +0900

    update guest prediction

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,10 +6,15 @@ class HomeController < ApplicationController
   def top
     # 全チケット数の指定
     @all_tickets = 100.0
+    # 請求済チケット数
     @request_tickets = Ticket.where(status: "配布待ち").count
+    # 配布済チケット数
     @get_tickets = Ticket.where(status: "配布済").count
+    # 販売完了済チケット数
     @sold_tickets = Ticket.where(status: "販売完了").count
+    # 請求状況円グラフ
     @data_request = {'請求済' => @request_tickets + @get_tickets + @sold_tickets, '未請求' => @all_tickets - @request_tickets - @get_tickets - @sold_tickets}
+    # 販売状況円グラフ
     @data_sold = {'販売完了済' => @sold_tickets, '未販売' => @all_tickets - @sold_tickets}
   end
 
