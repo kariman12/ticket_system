9e0dee819637cceb7dc49625771b3656ab787b49
kariman12 <marika.nakano.1212@gmail.com>
2020-09-26 04:31:58 +0900

    add chart group by dates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -16,6 +16,14 @@ class HomeController < ApplicationController
     @data_request = {'請求済' => @request_tickets + @get_tickets + @sold_tickets, '未請求' => @all_tickets - @request_tickets - @get_tickets - @sold_tickets}
     # 販売状況円グラフ
     @data_sold = {'販売完了済' => @sold_tickets, '未販売' => @all_tickets - @sold_tickets}
+    # chart用
+    @term_tickets = [{}, {}, {}]
+    @term_tickets[0]["name"] = "請求済チケット数"
+    @term_tickets[1]["name"] = "配布済チケット数"
+    @term_tickets[2]["name"] = "販売済チケット数"
+    @term_tickets[0]["tickets"] = Ticket.where(status: "配布待ち").or(Ticket.where(status: "配布済").or(Ticket.where(status: "販売完了")))
+    @term_tickets[1]["tickets"] = Ticket.where(status: "配布済").or(Ticket.where(status: "販売完了"))
+    @term_tickets[2]["tickets"] = Ticket.where(status: "販売完了")
   end
 
   def about
